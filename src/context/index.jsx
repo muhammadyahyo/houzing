022f67bfdc1170c83ea9ec2ebc5a99b9524720cf
muhammadyahyo <74825893+muhammadyahyo@users.noreply.github.com>
2023-08-03 10:46:56 +0500

    navbar is ready

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,15 +1,14 @@
-import {createContext} from 'react'
 import PropertiesProvider from './properties'
 
-const Root = createContext()
+
 
 
 const RootContext = ({children}) => {
-    return <Root.Provider>
+    return <>
         <PropertiesProvider>
             {children}
         </PropertiesProvider>
-    </Root.Provider>
+    </>
 }
 
 export default RootContext
